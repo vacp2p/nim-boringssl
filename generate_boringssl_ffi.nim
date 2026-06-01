2766689dd5268eff7ca7a41770fadc3da47be974
Richard Ramos <info@richardramos.me>
2026-06-01 12:54:40 -0400

    fix: do not auto map `ptr_diff` and enums

--- generate_boringssl_ffi.nim
+++ generate_boringssl_ffi.nim
@@ -2,11 +2,33 @@
 # Copyright (c) Status Research & Development GmbH 
 
 import futhark
+import std/json
 from os import parentDir, `/`
+
+type ptrdiff_t* {.importc: "ptrdiff_t", header: "<stddef.h>".} = int
+
+proc normalizeOpirImpl(opirOutput: JsonNode): JsonNode =
+  var resp = newJArray()
+  for node in opirOutput:
+    # The prelude owns ptrdiff_t through <stddef.h>. Dropping Futhark's
+    # typedef avoids a host-sized clong fallback during cross compilation.
+    if node["kind"].str == "typedef" and node.hasKey("name") and
+        node["name"].str == "ptrdiff_t":
+      continue
+
+    # enums are generated manually to avoid issue described in
+    # https://github.com/PMunch/futhark/issues/152
+    if node["kind"].str == "enum":
+      continue
+
+    resp.add node
+  resp
 
 importc:
   outputPath currentSourcePath.parentDir / "tmp_boringssl_ffi.nim"
   path currentSourcePath.parentDir / "boringssl/include"
+  addopircallback proc(opirOutput: JsonNode): JsonNode {.closure.} =
+    normalizeOpirImpl(opirOutput)
   rename FILE, CFile # Rename `FILE` that STB uses to `CFile` which is the Nim equivalent
   "openssl/aead.h"
   "openssl/hkdf.h"
